ea78a9aae0518c3ce6ad2bb6eef7ff49c3214afb
Samiul Himel <alamhpl11@gmail.com>
2025-05-31 04:02:01 +0600

    design

--- resources/views/backend/pages/package_service/events.blade.php
+++ resources/views/backend/pages/package_service/events.blade.php
@@ -2,14 +2,20 @@
 
 @section('content')
 
-<h1>Events</h1>
+<h1 class="mb-4" style="color:rgb(4, 38, 89);">Events</h1>
 
-@foreach($packages as $data)
-    <div class="">
-        <a style="color: black;" href="{{ route('admin.package.service.create', $data->id) }}">
-            <strong>{{ $data->name }}</strong>
-        </a>
-    </div>
-@endforeach
+<div class="row">
+    @foreach($packages as $data)
+        <div class="col-md-4 mb-4">
+            <div class="card shadow-sm border-0" style="background-color: #e6f0ff; border-radius: 10px;">
+                <div class="card-body text-center">
+                    <a href="{{ route('admin.package.service.create', $data->id) }}" style="text-decoration: none; color: #333;">
+                        <h5 class="card-title mb-0">{{ $data->name }}</h5>
+                    </a>
+                </div>
+            </div>
+        </div>
+    @endforeach
+</div>
 
 @endsection
