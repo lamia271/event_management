9bf0bbda5eca8c01581d003ad7b921d8e6d6b5fd
lamia271 <lamianazat205@gmail.com>
2025-06-01 23:05:53 +0600

    Random

--- resources/views/frontend/pages/sampleWork.blade.php
+++ resources/views/frontend/pages/sampleWork.blade.php
@@ -13,19 +13,19 @@
 		</div>
 		<div class="row">
 			<div class="col-lg-3 col-md-4 col-sm-6">
-               <img style="width: 300px;height:200px;" src="{{url('images/holud5.jpg')}}" alt="" class="img-fluid">
+               <img style="width: 300px;height:200px;" src="{{url('images/birthday.jpg')}}" alt="" class="img-fluid">
   
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6">
-                <img style="width: 300px;height:200px;" src="{{url('images/birthday1.jpg')}}" alt="" class="img-fluid">
+                <img style="width: 300px;height:200px;" src="{{url('images/marriage.jpg')}}" alt="" class="img-fluid">
   
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6">
-                <img style="width: 300px;height:200px;" src="{{url('images/marriage1.jpg')}}" alt="" class="img-fluid">
+                <img style="width: 300px;height:200px;" src="{{url('images/eng.jpg')}}" alt="" class="img-fluid">
   
             </div>
             <div class="col-lg-3 col-md-4 col-sm-6">
-                <img style="width: 300px;height:200px;" src="{{url('images/images (7).jpg')}}" alt="" class="img-fluid">
+                <img style="width: 300px;height:200px;" src="{{url('images/seminer.jpg')}}" alt="" class="img-fluid">
   
             </div>
 		
